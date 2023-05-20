73c7a112ce5416468b00648bed7c26dbd5524200
gregs <github@gregs.io>
2023-05-20 11:59:48 +0200

    adds unpacking method

--- panda_vs_stdlib_large.py
+++ panda_vs_stdlib_large.py
@@ -24,6 +24,12 @@ def merge_with_update():
 def merge_with_dict_comprehension():
     return {k: v for d in dictionaries for k, v in d.items()}
 
+def merge_with_unpacking():
+    result = {}
+    for d in dictionaries:
+        result = {**result, **d}
+    return result
+
 def merge_with_union():
     result = {}
     for d in dictionaries:
@@ -37,11 +43,13 @@ def merge_with_pandas():
 # Now we'll time each method
 update_time = timeit.timeit(merge_with_update, number=100)
 dict_comprehension_time = timeit.timeit(merge_with_dict_comprehension, number=100)
+unpacking_time = timeit.timeit(merge_with_unpacking, number=100)
 union_time = timeit.timeit(merge_with_union, number=100)
 pandas_time = timeit.timeit(merge_with_pandas, number=100)
 
 # And print the results
 print(f'update: {update_time} seconds')
 print(f'dict comprehension: {dict_comprehension_time} seconds')
+print(f'unpacking: {unpacking_time} seconds')
 print(f'union: {union_time} seconds')
 print(f'pandas: {pandas_time} seconds')
